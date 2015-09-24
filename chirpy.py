cf607107409ad4ea18e8f82a726f3f9f7430b7ac
Igor Gueths <igueths@lava-net.com>
2015-09-23 19:52:02 -0500

    Build parser for initially supported command line arguments.

--- chirpy.py
+++ chirpy.py
@@ -5,8 +5,8 @@ from collections import OrderedDict
 # Define our command line arguments and parse them.
 def parse_arguments():
     args = argparse.ArgumentParser(description='Manipulates Chirp-exported CSV files from the command line')
-    args.add_argument('csvfile', help='The filename of the CSV file we wish to use')
-    args.add_argument('outfile', help='The output CSV file we wish to write our output to.
+    args.add_argument('csvfile')
+    args.add_argument('outfile')
     args.add_argument('-l', '--location', help='The memory location in the radio for an entry', type=int)
     args.add_argument('-n', '--name', help='Name of this memory entry')
     args.add_argument('-f', '--frequency', help='Frequency associated with a memory location')
@@ -15,3 +15,8 @@ def parse_arguments():
     args.add_argument('-t', '--tonemode', help='Tone mode (PL | CTCSS | DTCS)', dest='tonemode', choices=['Tone', 'TSQL', 'DTCS'])
     args.add_argument('-T', '--rtonefreq', help='PL tone frequency, cannot be used unless mode tone is specified')
     args.add_argument('-c', '--ctonefreq', help='CTCSS tone frequency, cannot be used unless "TSQL" is passed to --tone')
+    args.add_argument('-m', '--mode', help='Signal mode (only FM is supported for now)')
+    args.parse_args()
+    return args
+
+parse_arguments()
